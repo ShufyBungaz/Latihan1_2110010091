4f35adcfcb52b5219aa60dfeb8edb5b88b307895
ShufyBungaz <auliyashufy64@gmail.com>
2023-06-12 15:28:11 +0800

    Membuat Class MatematikaBeraksi

--- src/hitung/matematika.java
+++ src/hitung/matematika.java
@@ -3,7 +3,7 @@ package hitung;
 public class matematika {
     private double bil1, bil2;
 
-    public Matematika(double bil1, double bil2) {
+    public matematika(double bil1, double bil2) {
         this.bil1 = bil1;
         this.bil2 = bil2;
     }
--- src/hitung/matematikaBeraksi.java
+++ src/hitung/matematikaBeraksi.java
@@ -2,5 +2,12 @@
 package hitung;
 
 public class matematikaBeraksi {
+    public static void main(String[] args) {
+        matematika shufy = new matematika(9,1);
     
+   System.out.println("Hasil Penjumlahan    ="+shufy.setPenjumlahan());
+   System.out.println("Hasil Pengurangan    ="+shufy.setPengurangan());
+   System.out.println("Hasil Perkalian   ="+shufy.setPerkalian());
+   System.out.println("Hasil Pembagian    ="+shufy.setPembagian());
+    }
 }
